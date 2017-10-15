3bedce742e2a431abcdf4a767b88c3f434d1a6e1
Sam Zierler <zierlers1@hawkmail.newpaltz.edu>
2017-10-15 18:39:58 -0400

    npm modules

---
+++ jquery-mockup/src/navigation.ts
@@ -0,0 +1,60 @@
+import * as $ from 'jquery';
+declare const navProperties: any;
+
+$(function() {
+    $("#header-placeholder").load("_header.html", function() {
+        $("#nav-placeholder").load(navProperties.mainNav, function() {
+            $(".nav-item").removeClass("active");
+            $(`#${navProperties.currentSection}-link`).addClass("active");
+        });
+    });
+
+    /* login.html *************************************************************** */
+    $("#loginUsernameRetrieve").on("click", function() {
+        $("#loginLoginRegisterPanel").hide();
+        $("#loginRecoverDetailsPanel").show();
+    });
+    $("#loginPasswordRetrieve").on("click", function() {
+        $("#loginLoginRegisterPanel").hide();
+        $("#loginRecoverDetailsPanel").show();
+    });
+    $("#loginCloseRecover").on("click", function() {
+        $("#loginRecoverDetailsPanel").hide();
+        $("#loginLoginRegisterPanel").show();
+    });
+    $("#loginRegisterBtn").on("click", function() {
+        $("#loginLoginRegisterPanel").hide();
+        $("#loginRegisterNewPanel").css("display", "flex");
+    });
+    $("#loginCloseRegisterBtn").on("click", function() {
+        $("#loginRegisterNewPanel").hide();
+        $("#loginLoginRegisterPanel").show();
+    });
+    /* routes.html *************************************************************** */
+    $("#addNewRouteBtn").on("click", function() {
+        $("#newRouteCard").toggle();
+    });
+    $("input[name=routetypes]").on("change", function() {
+        var type = $(this).val();
+        $("div.desc").hide();
+        $("#routesNewRouteGrade"+type).css({'display':"inline-block"});
+    });
+    $("input[value=YDS]").on("change", function() {
+        $("#topRopeCheck").show();
+        $("#routesNewRoutePitchesDiv").show();
+        $("#routeSafety").show();
+    });
+    $("input[value=VS]").on("change", function() {
+        $("#topRopeCheck").hide();
+        $("#routesNewRoutePitchesDiv").hide();
+        $("#routeSafety").hide();
+    });
+    $("#routedetails-flash, #routedetails-onsight").on("change", function() {
+        $("#routesNewRouteAttempts").prop("readonly", true);
+        $("#routesNewRouteAttempts").val("1");
+    });
+    $("#routedetails-send, #routedetails-project").on("change", function() {
+        $("#routesNewRouteAttempts").prop("readonly", false);
+        $("#routesNewRouteAttempts").val("");
+    });
+});
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require("express");
+const handler = require("./httpHandler.js")
+
+const server = http();
+
+server.use("/json", function(res, res, next) {
+    res.send({ happy: "Yom Kippur"})
+    next();
+});
+
+server.use("/old", handler.main);
+
+server.listen(3000);
+
+console.log("http://localhost:3000");
