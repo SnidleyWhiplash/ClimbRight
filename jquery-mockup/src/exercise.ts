151c969f4b76656a8ee165e479e97801ef0db77f
Sam Zierler <zierlers1@hawkmail.newpaltz.edu>
2017-10-19 19:04:41 -0400

    exercise.ts

--- jquery-mockup/src/exercise.ts
+++ jquery-mockup/src/exercise.ts
@@ -1,9 +1,12 @@
 import * as $ from 'jquery';
 
+var table_row_count = 0;
+
 export class Exercise {
     name: string;
     reps: number;
     sets: number;
+    time: string;
 
     static fetchDetails(): Exercise {
         var exercise = new Exercise();
@@ -11,13 +14,16 @@ export class Exercise {
         exercise.name = $("#trackerSelectExerciseDropdown").val().toString();
         exercise.reps = parseFloat($("#trackerRepCount").val().toString());
         exercise.sets = parseFloat($("#trackerSetCount").val().toString());
-        
+        exercise.time = $("#trackerTime").val().toString();        
+
         return exercise;
     }
 
     static logExercise(exercise: Exercise) {
-        var markup = "<tr class='table-dark'><th scope='row'></th><td>" + exercise.name + "</td><td>" 
-            + exercise.reps + "</td><td>" + exercise.sets + "</td></tr>"
+        table_row_count++;
+        var markup = "<tr class='table-dark'><th scope='row'>" + table_row_count + "</th><td>" 
+        + exercise.name + "</td><td>" + exercise.reps + "</td><td>" + exercise.sets + "</td><td>" 
+        + exercise.time + "</td><td><input type='checkbox' name='remove'></td></tr>"
         $("#myExercisesTable tbody").append(markup);
     }
 }
@@ -29,4 +35,13 @@ var exercise = new Exercise();
 $("#addExercise").click(function() {
     exercise = Exercise.fetchDetails();
     Exercise.logExercise(exercise);
+});
+
+$("#trackerDeleteChecked").click(function() {
+    $("#myExercisesTable tbody").find("input[name='remove']").each(function() {
+        if($(this).is(":checked")) {
+            table_row_count--;
+            $(this).parents("tr").remove();
+        }
+    });
 });
